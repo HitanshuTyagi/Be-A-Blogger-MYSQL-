7ec22a8c3c7cae87b9e357d1e0df084081403c24
Hitanshu Tyagi <hitanshu0523.be23@chitkara.edu.in>
2026-04-24 20:24:58 +0530

    fix ssl issue

--- config/database.js
+++ config/database.js
@@ -1,3 +1,21 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306,
+//     dialect: 'mysql',
+//     logging: false, // Set to console.log to see SQL queries
+//   }
+// );
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -9,7 +27,14 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    logging: false, // Set to console.log to see SQL queries
+    logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
